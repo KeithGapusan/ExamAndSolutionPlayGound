3f383cab8267e92f9bd04fda244b08f839013785
keith31 <ksgapusan@straightarrow.com.ph>
2018-09-24 12:54:45 +0800

    add perm solution

--- MyPlayground.playground/Pages/Permutation.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Permutation.xcplaygroundpage/Contents.swift
@@ -4,7 +4,67 @@ import Foundation
 
 var str = "Hello, playground"
 var data = [1,3,4]
+var data2 = ["A", "B", "C"]
 permutation(data)
+permutation(data2)
+//print(data)
+
+var arr = Array("ABC".characters)
+//permutations(arr.count, &arr)
+permutation(length : arr.count , charArray: arr)
+print("you")
+
+
+//func permutations(_ n:Int, _ a: inout Array<Character>) {
+//    if n == 1 {print(a); return}
+//    for i in 0..<n-1 {
+//        permutations(n-1,&a)
+//        a.swapAt(n-1, (n % 2 == 1) ? 0 : i)
+//    }
+//    permutations(n-1,&a)
+//}
+
+
+func permutation(length : Int, charArray: [Character]){
+    var temp = charArray
+    if length  == 1{
+        print(charArray)
+        return
+    }
+    for i in 0..<length - 1 {
+        permutation(length: length - 1 , charArray: temp)
+        temp.swapAt(length - 1, (length % 2 == 1) ? 0 : i)
+    }
+    permutation(length: length - 1, charArray: temp)
+}
+
+func permutation(_ A : [String]) -> [String]{
+    var result = [String]()
+    let numOfElements = A.count
+    var index = 0
+    var temp = ""
+    for content in A{
+        print(index)
+        if (numOfElements - 1) == index{
+            temp = temp + content
+            print("content \(temp)")
+            if result.contains(temp){
+                //swap
+            }else{
+                //add to result
+                result.append(temp)
+            }
+        }else{
+            temp = temp + content
+        }
+        index = index + 1
+    }
+    
+    return result
+}
+
+
+
 
 //: [Next](@next)
 func permutation(_ A : [Int]) -> Int {
